574b3e53087db086894d739612ba1727aee03bc9
Dandara <dandara.dascanio@gmail.com>
2020-08-13 21:45:28 -0300

    adicionado o site dandara

---
+++ site_dandara/ex4.js
@@ -0,0 +1,7 @@
+// 4. Colocar uma opção para o carrinho ser entregue (usar promise e timeout)
+//    (timeout para demorar um pouco para entregar)
+//    Quando o usuário escolher que quer uma entrega, criar uma promise para realizar essa entrega e
+//    agradecer pela compra logo depois da promise
+//    "Agendamos sua entrega"
+//    "Agradecemos sua compra!"
+//    alguns segundos depois, com a promise concluída, aparecerá uma mensagem de "Carrinho entregue"
